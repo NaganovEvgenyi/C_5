528bee54588c2a8f2269451c71d238f3c2f21aaa
NaganovEvgenyi <enaganov@inbox.ru>
2022-10-29 21:33:55 +0300

    3 задача

--- Program.cs
+++ Program.cs
@@ -25,8 +25,7 @@
      }
      return res;
  }
- */
-
+ 
   //Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
  //[3, 7, 23, 12] -> 19
  //[-4, -6, 89, 6] -> 0
@@ -51,3 +50,41 @@
      }
      return res;
  }
+*/
+
+  // Задайте массив вещественных чисел(просто от минус бесконечности, до плюс бесконечности, с дробными не работаем).
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array2 = GetArray(10, 0, 10);
+double max = 0;
+double min = 10;
+Console.WriteLine($"Вот массив:{String.Join(", ",array2)}");
+for (int i = 0; i < array2.Length; i++)
+{
+    if (array2[i] > max)
+    {
+        max = array2[i];
+        Console.WriteLine($"макс {max}");
+    }
+    if (array2[i] < min)
+    {
+        min = array2[i];
+        Console.WriteLine($"мин {min}");
+    }
+}
+
+Console.WriteLine($"Разница между макс. и мин. значением {max - min}");
+
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = Convert.ToDouble(new Random().Next(minValue, maxValue + 1));
+
+    }
+    return res;
+}
